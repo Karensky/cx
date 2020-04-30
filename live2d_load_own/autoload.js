3c3cdca3367abf12b1baebc94c19e2f9c126fdc5
辰旭 <59323207+Karensky@users.noreply.github.com>
2020-04-30 10:47:42 +0800

    Update and rename live2d_load_own/autoload.js to  live2d_load_own/autoload.js

--- live2d_load_own/autoload.js
+++ live2d_load_own/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径，记得修改哦
-const live2d_path = "https://你的域名/live2d_load_own/";
-const tips_path = "https://你的域名/live2d_load_own/";
+const    live2d_path    =    “   https：// karen.shiningstarrysky.cn / live2d_load_own /”;
+const tips_path = "https://你karen.shiningstarrysky.cnlive2d_load_own/";
 //const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
@@ -34,7 +34,7 @@ function loadExternalResource(url, type) {
 	]).then(() => {
 		initWidget({
 			waifuPath: tips_path + "waifu-tips.min.json",
-			apiPath: "https://你的域名/live2d_api/",
+			apiPath: "https://karen.shiningstarrysky.cn/live2d_api/",
 			//cdnPath: "https://你的CDN域名/live2d_api/"从本地加载此项不用管
 		});
 	});
